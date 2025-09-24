42414022a9d860572ac57b30fe08e99fd483ec64
Manioker <148788622+Manioker@users.noreply.github.com>
2025-09-24 16:30:58 +0200

    Add files via upload

--- srcs/requirements/WordPress/auto_config.sh
+++ srcs/requirements/WordPress/auto_config.sh
@@ -1,17 +1,19 @@
 #!/bin/sh
 
-sleep 1
+sleep 5
 
 echo "[ INFO ] Starting WordPress..."
 
-if [ ! -f "/wp-config.php" ]; then
+if [ ! -f "/var/www/html/wp-config.php" ]; then
 	wp config create \
 		--allow-root \
 		--dbname=$SQL_DATABASE \
 		--dbuser=$SQL_USER \
 		--dbpass=$SQL_PASSWORD \
-		--dbhost=mariadb:3306 --path='/var/www/wordpress'
+		--dbhost=mariadb:3306 --path='/var/www/html'
 fi
 
-echo "[ INFO ] Running "
+cp /path/to/hello.html /var/www/html/hello.html
+
+echo "[ INFO ] Running PHP-FPM..."
 exec php-fpm84 -F
